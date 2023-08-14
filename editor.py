3868e6b8f0bfbf84828c331f64d267bfa6abfe47
Andre Gallegos Quenaya <andre.gallegos@tecsup.edu.pe>
2023-08-13 20:40:36 -0500

    Adding the menu and modify the settings.py

--- editor.py
+++ editor.py
@@ -1,13 +1,15 @@
-import pygame, sys
+import pygame
+import sys
 from pygame.math import Vector2 as vector
-from pygame.mouse import get_pressed as mouse_buttons
 from pygame.mouse import get_pos as mouse_pos
+from pygame.mouse import get_pressed as mouse_buttons
+
+from menu import Menu
 from settings import *
 
 
 class Editor:
     def __init__(self):
-
         # main setup
         self.display_surface = pygame.display.get_surface()
 
@@ -21,18 +23,27 @@ class Editor:
         self.support_line_surf.set_colorkey('green')
         self.support_line_surf.set_alpha(30)
 
-    # Input
+        # Selection
+        self.selection_index = 2
+
+        # Menu
+        self.menu = Menu()
+
+    # input
     def event_loop(self):
         # Explain: pygame.event.get() returns a list of all events
-        # that have happened since the last time this was called.
+        # that happened since the last time pygame.event.get() was called.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             self.pan_input(event)
+            self.selection_hotkeys(event)
+            self.menu_click(event)
 
     def pan_input(self, event):
 
+        # middle mouse button pressed / released
         if event.type == pygame.MOUSEBUTTONDOWN and mouse_buttons()[1]:
             self.pan_active = True
             # distance between mouse and origin
@@ -41,24 +52,35 @@ class Editor:
         if not mouse_buttons()[1]:
             self.pan_active = False
 
-        # mouse wheel to move origin to the left, right, up, or down
+        # mouse wheel
         if event.type == pygame.MOUSEWHEEL:
             if pygame.key.get_pressed()[pygame.K_LCTRL]:
                 self.origin.y -= event.y * 50
             else:
                 self.origin.x -= event.y * 50
 
-        # Panning update
+        # panning update
         if self.pan_active:
             self.origin = vector(mouse_pos()) - self.pan_offset
 
-    # Drawing the grid
-    def draw_tile_lines(self):
-        cols = WINDOW_WIDTH // TILE_SIZE  # 20
-        rows = WINDOW_HEIGHT // TILE_SIZE  # 15
+    def selection_hotkeys(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                self.selection_index += 1
+            if event.key == pygame.K_LEFT:
+                self.selection_index -= 1
+        # Limiting the selection index based in the range of 2 to 18
+        self.selection_index = max(2, min(self.selection_index, 18))
 
-        # offset from origin to the top left corner of the screen
-        # origin_offset is the distance from the origin to the top left corner of the screen
+    def menu_click(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN and self.menu.rect.collidepoint(mouse_pos()):
+            self.selection_index = self.menu.click(mouse_pos(), mouse_buttons())
+
+    # drawing
+    def draw_tile_lines(self):
+        cols = WINDOW_WIDTH // TILE_SIZE
+        rows = WINDOW_HEIGHT // TILE_SIZE
+
         origin_offset = vector(
             x=self.origin.x - int(self.origin.x / TILE_SIZE) * TILE_SIZE,
             y=self.origin.y - int(self.origin.y / TILE_SIZE) * TILE_SIZE)
@@ -78,7 +100,8 @@ class Editor:
     def run(self, dt):
         self.event_loop()
 
-        # Drawing
-        self.display_surface.fill('white')
+        # drawing
+        self.display_surface.fill('gray')
         self.draw_tile_lines()
         pygame.draw.circle(self.display_surface, 'red', self.origin, 10)
+        self.menu.display(self.selection_index)
